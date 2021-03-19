eff214424e8ae36a51f237ca58b61943f814b6c2
Abira16 <47822500+Abira16@users.noreply.github.com>
2021-03-19 13:03:49 +0530

    Update store.ts

--- sdk/src/store.ts
+++ sdk/src/store.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
  * WSO2 Inc. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
@@ -57,5 +57,3 @@ const store = new LocalStorage();
 
 // Instantiate the AsgardeoAuthClient and pass the store object as an argument into the constructor of the asgardeo-auth-js sdk.
 export const auth = new AsgardeoAuthClient(store);
-
-
